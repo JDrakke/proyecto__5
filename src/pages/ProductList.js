65347eceeb6e251bd52608414cff2a3dae471afd
Jeremy Hernandez Calderon <jehernandez.dwfs11@bootcampudd.cl>
2024-08-10 13:53:31 -0400

    Describe los cambios realizados

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -26,7 +26,7 @@ const ProductList = () => {
       <div className="product-grid">
         {products.map(product => (
           <div className="product-card" key={product._id}>
-            <img src={`/assets/${product.image}`} alt={product.name} className="product-image" />
+            <img src={`${process.env.PUBLIC_URL}/assets/${product.image}`} alt={product.name} className="product-image" />
             <div className="product-info">
               <h3 className="product-name">{product.name}</h3>
               <p className="product-price">${product.price.toFixed(2)}</p>
